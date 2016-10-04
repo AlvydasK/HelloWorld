e99b71c08cccc23ebdbc1691fc0b5f5f3e3101f9
EivydasK <EivydasK@VDISAF002>
2016-10-04 10:57:58 +0300

    Improve stability

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,7 +6,14 @@ namespace HelloWorld
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           
+            if(args.Length < 1)
+            {
+                Console.WriteLine("Program requires at least 1 argument");
+                return;
+            }
+
+            Console.WriteLine(string.Format("Hello {0}!", args[]));
 
             Console.ReadLine();
         }
